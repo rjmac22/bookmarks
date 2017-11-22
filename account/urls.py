c374c80b5b467b648073bf2e2cec32db25764650
Rob Maclean <rjmac@live.co.uk>
2017-11-22 19:35:49 +0000

    add authentication views

---
+++ account/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    # previous login view
+    #url(r'^login/$', views.user_login, name='login'),
+
+    # url(r'^login/$', views.user_login, name='login'),
+    url(r'^$', views.dashboard, name='dashboard'),
+
+    # login /logout urls
+    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login',
+            name='logout.logout_then_login'),
+
+]
