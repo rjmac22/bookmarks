5b2e6690fd377129c386da03d23387a769df2607
Rob Maclean <rjmac@live.co.uk>
2017-11-24 18:38:02 +0000

    add user model

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # previous login view
@@ -14,4 +16,24 @@ urlpatterns = [
     url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login',
             name='logout.logout_then_login'),
 
+    # change password urls
+    url(r'^password-change/$', 'django.contrib.auth.views.password_change',
+         name='password_change'),
+    url(r'^password-change/done/$', 'django.contrib.auth.views.password_change_done',
+           name="password_change_done"),
+
+    # restore password urls
+    url(r'^password-reset/$', 'django.contrib.auth.views.password_reset',
+           name="password_reset"),
+    url(r'^password-reset/done/$', 'django.contrib.auth.views.password_reset_done',
+          name="password_reset_done"),
+    url(r'^password_reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
+         'django.contrib.auth.views.password_reset_confirm', name="password_reset_confirm"),
+    url(r'^password_reset/complete/$', 'django.contrib.auth.views.password_reset_complete',
+       name="password_reset_complete")
+
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -104,8 +104,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 from django.core.urlresolvers import reverse_lazy
 LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
 LOGIN_URL = reverse_lazy('login')
 LOGOUT_URL = reverse_lazy('logout')
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
