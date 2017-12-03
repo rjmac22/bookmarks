5fdb8fe4bb0cc402bd56d67e2452184dc6d6e290
Rob Maclean <rjmac@live.co.uk>
2017-12-03 20:04:41 +0000

    add image model and views

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -21,4 +21,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^account/', include('account.urls')),
+    url(r'^images/', include('images.urls', namespace='images')),
+    url(r'^images/', include('images.urls', namespace='images')),
 ]
