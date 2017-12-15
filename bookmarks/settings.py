1220091204e298bf82fa5b512ccf404ee58134a4
Rob Maclean <rjmac@live.co.uk>
2017-12-15 20:23:34 +0000

    add ajax and custom decorators

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social.apps.django_app.default',
-    'images'
+    'images',
+    'sorl.thumbnail'
 
 )
 
--- images/views.py
+++ images/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
+from .models import Image
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
 
 
 @login_required
@@ -27,3 +31,28 @@ def image_create(request):
         return render(request, 'images/image/create.html',
                       {'section': 'images',
                       'form': form})
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,
+                  'Images/image/detail.html',
+                  {'section': 'images',
+                  'image': image})
+
+@ajax_required
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action =='like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+                return JsonResponse({'status': 'ok' })
+        except:
+            pass
+        return JsonResponse({'status': 'ok'})
